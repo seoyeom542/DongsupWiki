afbb17e4a8f0bae06e91cfc7c99580a3aeeff8ca
덩어리 <seoyeom542@deong-eoliui-iMac.local>
2024-07-12 02:08:12 +0900

    prefix 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 
+const prefix =
+  process.env.NODE_ENV === "production"
+    ? "https://seoyeom542.github.io/dongsupwiki"
+    : "";
+
 const nextConfig = {
   output: "export",
   assetPrefix: prefix,
-  basePath: "/dongsupwiki",
 };
 
 export default nextConfig;
