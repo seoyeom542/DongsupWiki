e19ab93045e0db1bffe81e412ead439dbf3dbb78
덩어리 <seoyeom542@deong-eoliui-iMac.local>
2024-07-12 01:18:49 +0900

    favicon 적용

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,6 +9,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "dongsupwiki",
   description: "animal Forest Encyclopedia",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({ children }) {
