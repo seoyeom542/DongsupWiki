a20fbd8beaed98021007c794fb8d3aa1c68d1909
덩어리 <seoyeom542@deong-eoliui-iMac.local>
2024-07-11 22:48:13 +0900

    header 추가

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import Header from "@/components/Header/page";
 import Footer from "@/components/Footer/page";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Header />
         {children}
         <Footer />
       </body>
