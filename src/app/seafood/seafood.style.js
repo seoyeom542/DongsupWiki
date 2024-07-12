5663f70652667e32d8744d7ffe8db18b3997228e
덩어리 <seoyeom542@deong-eoliui-iMac.local>
2024-07-13 00:04:16 +0900

    해산물 페이지 style 추가

---
+++ src/app/seafood/seafood.style.js
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled";
+import media from "@shared/mediaQuery";
+
+const SeafoodStyle = styled.div`
+  ${media.desktop} {
+    width: 1080px;
+    margin: 0 auto;
+  }
+`;
+
+export default SeafoodStyle;
