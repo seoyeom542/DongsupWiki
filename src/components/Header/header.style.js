edc10445a48f587582c542ea761ee5985c7d797a
덩어리 <seoyeom542@deong-eoliui-iMac.local>
2024-07-12 15:57:55 +0900

    desktop header css 수정

--- src/components/Header/header.style.js
+++ src/components/Header/header.style.js
@@ -3,26 +3,28 @@ import media from "@shared/mediaQuery";
 
 const HeaderStyle = styled("header")`
   background-color: #bad647;
-  margin: 0 auto;
-  display: flex;
-  height: 75px;
-  ${media.desktop} {
-    width: 1060px;
-  }
-  .header--logo {
-    height: 100%;
-  }
-  .header--menuWrapper {
-    display: flex;
-    &__menu {
-      display: block;
-      text-decoration: none;
-      font-size: 40px;
-      line-height: 75px;
-      color: #fffae1;
+  .header {
+    ${media.desktop} {
+      height: 75px;
+      display: flex;
+      width: 1060px;
+      margin: 0 auto;
     }
-    &__menu:not(:last-child) {
-      margin-right: 20px;
+    &--logo {
+      height: 100%;
+    }
+    &--menuWrapper {
+      display: flex;
+      &__menu {
+        display: block;
+        text-decoration: none;
+        font-size: 40px;
+        line-height: 75px;
+        color: #fffae1;
+      }
+      &__menu:not(:last-child) {
+        margin-right: 20px;
+      }
     }
   }
 `;
--- src/components/Header/page.js
+++ src/components/Header/page.js
@@ -5,22 +5,24 @@ import HeaderWrapper from "./header.style";
 const Header = () => {
   return (
     <HeaderWrapper>
-      <Link href="/">
-        <img src="/logo.png" alt="logo" className="header--logo" />
-      </Link>
-      <div className="header--menuWrapper">
-        <Link href="/fish" className="header--menuWrapper__menu">
-          물고기
+      <div className="header">
+        <Link href="/">
+          <img src="/logo.png" alt="logo" className="header--logo" />
         </Link>
-        <Link href="/insect" className="header--menuWrapper__menu">
-          곤충
-        </Link>
-        <Link href="/seafood" className="header--menuWrapper__menu">
-          해산물
-        </Link>
-        <Link href="/artwork" className="header--menuWrapper__menu">
-          미술품
-        </Link>
+        <div className="header--menuWrapper">
+          <Link href="/fish" className="header--menuWrapper__menu">
+            물고기
+          </Link>
+          <Link href="/insect" className="header--menuWrapper__menu">
+            곤충
+          </Link>
+          <Link href="/seafood" className="header--menuWrapper__menu">
+            해산물
+          </Link>
+          <Link href="/artwork" className="header--menuWrapper__menu">
+            미술품
+          </Link>
+        </div>
       </div>
     </HeaderWrapper>
   );
