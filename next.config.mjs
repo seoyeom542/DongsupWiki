6e018df4f34dc0c3c343abd79cdcb2e734c6d118
덩어리 <seoyeom542@deong-eoliui-iMac.local>
2024-07-12 21:30:50 +0900

    사용하지 않는 변수 제거

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-const prefix =
-  process.env.NODE_ENV === "production"
-    ? "https://seoyeom542.github.io/dongsupwiki"
-    : "";
-
 const nextConfig = {
   output: "export",
   basePath: "/dongsupwiki",
