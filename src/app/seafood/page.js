358167d62da6934824ac14c0a968e377beb4eeda
덩어리 <seoyeom542@deong-eoliui-iMac.local>
2024-07-13 00:54:38 +0900

    해산물 테이블 추가

--- src/app/seafood/page.js
+++ src/app/seafood/page.js
@@ -1,10 +1,40 @@
 "use client";
+import SeafoodData from "../../../data/seafood";
 import SeafoodWrapper from "./seafood.style";
+import nextConfig from "@config";
 
 const Seafood = () => {
+  const seafooddata = SeafoodData;
   return (
     <SeafoodWrapper>
-      <div>해산물 티비</div>
+      <div>해산물 리스트</div>
+      <table>
+        <th>
+          <td>번호</td>
+          <td>이름</td>
+          <td>이미지</td>
+          <td>출현 기간</td>
+          <td>채집 상태</td>
+          <td>기증 상태</td>
+        </th>
+        {seafooddata.map((data, index) => {
+          return (
+            <tr key={index}>
+              <td>{index + 1}</td>
+              <td>{data.name}</td>
+              <td>
+                <img
+                  src={`${nextConfig.basePath}` + data.src}
+                  alt={data.name}
+                />
+              </td>
+              <td>{data.name}</td>
+              <td>{data.collectionStatus ? "채집완료" : "미채집"}</td>
+              <td>{data.donationStatus ? "기증" : "기증전"}</td>
+            </tr>
+          );
+        })}
+      </table>
     </SeafoodWrapper>
   );
 };
