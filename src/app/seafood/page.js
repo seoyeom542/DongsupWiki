a95d0f32c13d0e6775f35d1585cc011b7d952c2e
덩어리 <seoyeom542@deong-eoliui-iMac.local>
2024-07-13 23:43:15 +0900

    onclick 동작 추가

--- src/app/seafood/page.js
+++ src/app/seafood/page.js
@@ -1,45 +1,61 @@
 "use client";
+import { useState } from "react";
+
 import SeafoodData from "../../../data/seafood";
 import SeafoodWrapper from "./seafood.style";
 import nextConfig from "@config";
 
 const Seafood = () => {
   const seafooddata = SeafoodData;
+  const [openPopup, setOpenPopup] = useState(false);
 
   const showInfo = () => {
     console.log("showInfo");
+    const isOpen = !openPopup;
+    setOpenPopup(isOpen);
   };
 
   return (
     <SeafoodWrapper>
       <table className="seafood__table">
-        <tr>
-          <th>번호</th>
-          <th>이름</th>
-          <th>이미지</th>
-          <th>상세보기</th>
-        </tr>
-        {seafooddata.map((data, index) => {
-          return (
-            <tr key={index}>
-              <td className="seafood__table--index">{index + 1}</td>
-              <td className="seafood__table--name">{data.name}</td>
-              <td>
-                <img
-                  className="seafood__table--img"
-                  src={`${nextConfig.basePath}` + data.src}
-                  alt={data.name}
-                />
-              </td>
-              <td>
-                <button className="seafood__table--button" onClick={showInfo}>
-                  상세보기
-                </button>
-              </td>
-            </tr>
-          );
-        })}
+        <thead>
+          <tr>
+            <th>번호</th>
+            <th>이름</th>
+            <th>이미지</th>
+            <th>상세보기</th>
+          </tr>
+        </thead>
+        <thead>
+          {seafooddata.map((data, index) => {
+            return (
+              <tr key={index}>
+                <td className="seafood__table--index">{index + 1}</td>
+                <td className="seafood__table--name">{data.name}</td>
+                <td>
+                  <img
+                    className="seafood__table--img"
+                    src={`${nextConfig.basePath}` + data.src}
+                    alt={data.name}
+                  />
+                </td>
+                <td>
+                  <button className="seafood__table--button" onClick={showInfo}>
+                    상세보기
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </thead>
       </table>
+
+      {openPopup && (
+        <div className="fursysQuestionaction--popup-wrap">
+          팝업노출!!!
+          {/* <InquiryActionPopup clickCancle={clickQuestion} /> */}
+        </div>
+      )}
     </SeafoodWrapper>
   );
 };
